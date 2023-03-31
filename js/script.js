1b20639afb43afc3177e34c088f5096f3cffafd2
pranjaljaiswal35@gmail.com <pjpranjaljaiswal04@gmail.com>
2023-03-31 11:17:20 +0530

    refactor:optimizing code

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
 const input = document.querySelector("input");
 const button = document.querySelector(".btn");
 const ul = document.querySelector(".myul");
+
+// Cache frequently used elements
 const li = ul.children;
-const cross = document.getElementsByClassName("cross");
+const cross = ul.getElementsByClassName("cross");
+
 button.addEventListener("click", (event) => {
   event.preventDefault();
   let task = document.createTextNode(input.value);
   let li = document.createElement("li");
   let span = document.createElement("span");
   if (input.value === "") {
-    alert("Please!Write some task");
+    alert("Please!Write some task");//if input field is empty
   } else {
     li.appendChild(task);
     const myclasses = ["list", "list:hover"];
-    li.classList.add(...myclasses);
+    li.classList.add(...myclasses);//using spread operator 
     let txt = document.createTextNode("x");
-    span.appendChild(txt);
+    span.appendChild(txt)
     const classes = ["cross", "cross:hover"];
     span.classList.add(...classes);
     li.appendChild(span);
     ul.appendChild(li);
-    input.value = "";
+    input.value = "";//make empty for another task
   }
-  span.onclick = function () {
-    li.style.display = "none";
-  };
-  li.onclick = function () {
-    li.classList.toggle("checked");
-  };
 });
-for (let i = 0; i < cross.length; i++) {
-  cross[i].onclick = function (e) {
-    e.target.parentElement.style.display = "none";
-  };
-}
-for (let i = 0; i < li.length; i++) {
-  li[i].onclick = function (event) {
+
+// event delegation for li and cross button
+ul.addEventListener("click", (event) => {
+  if (event.target.classList.contains("cross")) {
+    event.target.parentElement.style.display = "none";//span is parentelement
+  } else if (event.target.tagName === "LI") {
     event.target.classList.toggle("checked");
-  };
-}
+  }
+});
+
+
